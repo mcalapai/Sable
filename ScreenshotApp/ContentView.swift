32e2652778714b2c2f0864cee39eba03a4f91f53
mcalapai <69445122+mcalapai@users.noreply.github.com>
2024-02-03 13:27:02 +1100

    Initial Commit

---
+++ ScreenshotApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScreenshotApp
+//
+//  Created by Matthew Calapai on 3/2/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScreenshotApp/ScreenshotAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenshotAppApp.swift
+//  ScreenshotApp
+//
+//  Created by Matthew Calapai on 3/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScreenshotAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
